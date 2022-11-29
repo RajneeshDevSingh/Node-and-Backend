0c876af2ee0a3b801ca839649bdc5c530d84d690
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2022-11-30 02:38:27 +0530

    first commit

---
+++ s1/data.js
@@ -0,0 +1,16 @@
+const data = [
+    {
+        name:"RDS",
+        Sid:5255,
+    },
+    {
+        name:"RDS",
+        Sid:5255,
+    },
+    {
+        name:"RDS",
+        Sid:5255,
+    }
+]
+
+module.exports = data;
---
+++ s1/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const data = require('./data');
+
+http.createServer((request , response)=>
+{
+    response.writeHead(200,{'Content-Type':'application/json'});
+    response.write(JSON.stringify(data));
+    response.end();
+}).listen(5000);
