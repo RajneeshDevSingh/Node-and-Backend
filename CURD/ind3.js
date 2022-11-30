7ea344698d05eeb837ce4624d37d9cd6e4d4d47a
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2022-11-30 23:55:38 +0530

    CURD3

--- CURD/ind3.js
+++ CURD/ind3.js
@@ -4,16 +4,29 @@ const path = require('path')
 const dirPath = path.join(__dirname );
 const filePath = `${dirPath}/data.txt`
 
-// fs.writeFileSync(filePath , "Some Data here")
+// fs.writeFileSync(filePath , " Some Data here")
 
 console.log(filePath)
 // console.log(dirPath)
 
 
-try {
-    const data = fs.readFileSync(filePath, 'utf8');
-    console.log(data);
-  } catch (err) {
-    console.error(err);
-  }
+// try {
+//     const data = fs.readFileSync(filePath, 'utf8');
+//     console.log(data);
+//   } catch (err) {
+//     console.error(err);
+//   }
 
+
+// fs.appendFile(filePath,"Some new Data" , (err)=>
+// {
+//     if(!err)console.log("File is Updated") ;
+// })
+
+// fs.rename(filePath ,`${dirPath}/NewData.txt`, (err)=>
+// {
+//     if(!err)console.log("File name is Updated") ;
+// }) 
+
+
+fs.unlinkSync(`${dirPath}/NewData.txt`)
