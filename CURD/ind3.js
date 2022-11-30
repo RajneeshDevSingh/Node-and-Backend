5dcf773a070eead1ac18a8202e5fae41a6d8722d
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2022-11-30 23:38:02 +0530

    CURD2

--- CURD/ind3.js
+++ CURD/ind3.js
@@ -4,8 +4,16 @@ const path = require('path')
 const dirPath = path.join(__dirname );
 const filePath = `${dirPath}/data.txt`
 
-fs.writeFileSync(filePath , "Some Data here")
+// fs.writeFileSync(filePath , "Some Data here")
 
 console.log(filePath)
-console.log(dirPath)
+// console.log(dirPath)
+
+
+try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
 
