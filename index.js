61e1ddfbc7f670da5ea2dd0b3da265c395c4d306
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2022-12-02 00:23:03 +0530

    2

--- index.js
+++ index.js
@@ -4,6 +4,33 @@ const path = require('path')
 const app = express();
 const publicPath = path.join(__dirname , 'public')
 
-app.use(express.static(publicPath))
-console.log(publicPath)
+// app.use(express.static(publicPath))
+// console.log(publicPath)
+// app.listen(5000)
+
+
+app.set('view engine' , 'ejs');
+
+app.get('' , (req , resp) =>
+{
+    resp.sendFile(`${publicPath}/index.html`)
+});
+
+
+app.get('/help',(req , resp)=>
+{
+    resp.sendFile(`${publicPath}/help.html`)
+});
+
+app.get('/profile' , (req , resp)=>
+{
+    const user = 
+    {
+        name : 'Rajneesh',
+        email:'@Rajneesh.com',
+    }
+
+    resp.render('profile' , {user})
+})
+
 app.listen(5000)
