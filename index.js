06d654ae033136f2b74ddcbc9a7818ec5e0422b5
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2022-12-05 12:12:49 +0530

    3

--- index.js
+++ index.js
@@ -1,36 +1,59 @@
-const express = require('express')
-const path = require('path')
+// const express = require('express')
+// const path = require('path')
 
-const app = express();
-const publicPath = path.join(__dirname , 'public')
+// const app = express();
+// const publicPath = path.join(__dirname , 'public')
 
-// app.use(express.static(publicPath))
-// console.log(publicPath)
+// // app.use(express.static(publicPath))
+// // console.log(publicPath)
+// // app.listen(5000)
+
+
+// app.set('view engine' , 'ejs');
+
+// app.get('' , (req , resp) =>
+// {
+//     resp.sendFile(`${publicPath}/index.html`)
+// });
+
+
+// app.get('/help',(req , resp)=>
+// {
+//     resp.sendFile(`${publicPath}/help.html`)
+// });
+
+// app.get('/profile' , (req , resp)=>
+// {
+//     const user = 
+//     {
+//         name : 'Rajneesh',
+//         email:'@Rajneesh.com',
+//     }
+
+//     resp.render('profile' , {user})
+// })
+
 // app.listen(5000)
 
 
-app.set('view engine' , 'ejs');
 
-app.get('' , (req , resp) =>
+
+// ####################################################
+// connection with mongo
+
+const {MongoClient} = require("mongodb");
+
+const url  = 'mongodb://localhost:27017'
+const dataBase = 'e-comm'
+const client = new MongoClient(url)
+
+async function getData()
 {
-    resp.sendFile(`${publicPath}/index.html`)
-});
+    let result =await client.connect();
+    let db = result.db(dataBase)
+    let collection = db.collection('products')
+    let response = await collection.find({}).toArray();
+    console.log(response)
+}
 
-
-app.get('/help',(req , resp)=>
-{
-    resp.sendFile(`${publicPath}/help.html`)
-});
-
-app.get('/profile' , (req , resp)=>
-{
-    const user = 
-    {
-        name : 'Rajneesh',
-        email:'@Rajneesh.com',
-    }
-
-    resp.render('profile' , {user})
-})
-
-app.listen(5000)
+getData();
